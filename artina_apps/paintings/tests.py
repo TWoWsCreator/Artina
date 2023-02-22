d96574c910db3095e46d4e70612f8520f00514af
Danila Eremin <danilaeremin@google.com>
2023-02-22 23:07:03 +0100

    Fix running tests in paintings app

--- artina_apps/paintings/tests.py
+++ artina_apps/paintings/tests.py
@@ -1,16 +1,20 @@
-# from django.test import Client, TestCase
+from django.test import Client, TestCase
 
-# from django.urls import reverse
+from django.urls import reverse
 
-# from parameterized import parameterized
+from parameterized import parameterized
 
 
-# class StaticURLTests(TestCase):
-#     @parameterized.expand(
-#         [
-#             ('1', 200)
-#         ]
-#     )
-#     def test_painting_endpoint(self):
-#         response = Client().get('/paintings/on_wild_north/')
-#         self.AssertEqual(response.status_code, 200)
+class StaticURLTests(TestCase):
+    fixtures = [
+        'fixtures/data.json',
+    ]
+
+    @parameterized.expand(
+        [
+            ('on_wild_north', 200),
+        ]
+    )
+    def test_painting_endpoint(self, name, expected_status):
+        response = Client().get(f'/paintings/{name}/')
+        self.assertEqual(response.status_code, expected_status)
